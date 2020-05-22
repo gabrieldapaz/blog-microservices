bf2c76623ca21e8aee2a38e09567ffdf1c5902f2
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-22 17:27:24 -0300

    add comment component to App.js

--- client/src/PostLists.js
+++ client/src/PostLists.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import CommentCreate from './CommentCreate';
 
 export default () => {
   const [posts, setPosts] = useState({});
@@ -12,11 +13,30 @@ export default () => {
     setPosts(res.data);
   };
 
-  //Guarantee that fetchPosts will only run after our component first displayed
-  //on the screen
+  //Guarantee that fetchPosts will only run after our component first displayed on the screen.
+  //The empty array guarantee the the function will run only one time
   useEffect(() => {
     fetchPosts();
   }, []);
 
-  return <div />;
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: '30%', marginBottom: '20px' }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentCreate postId={post.id} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
 };
