c1ee4e892450258c71736dfcfa139129e13d8b74
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-22 18:57:15 -0300

    add Comment List to the App

--- client/src/PostLists.js
+++ client/src/PostLists.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default () => {
   const [posts, setPosts] = useState({});
@@ -28,6 +29,7 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
