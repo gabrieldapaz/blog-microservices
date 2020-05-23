f17aca2a25df88de8c45add2c866eefbb5a7d281
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-23 12:36:48 -0300

    create handleEvent function

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,8 +7,12 @@ const app = express();
 // incoming request stream and exposes it on req.body.
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   // Missing handling code to avoid one of them fall.
   axios.post('http://localhost:4000/events', event);
@@ -17,6 +21,10 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
